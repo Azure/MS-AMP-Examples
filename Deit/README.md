2e413cbad9cf7963ed1f6cf96ef42ff41ff20ebb
tocean <yuxiangyang@microsoft.com>
2023-04-13 07:35:48 +0000

    Add deit examples

---
+++ Deit/README.md
@@ -0,0 +1,9 @@
+Run deit:
+```
+sh run.sh
+```
+
+Run deit with ms-amp:
+```
+sh run_msamp.sh
+```
---
+++ Deit/run_msamp.sh
@@ -0,0 +1,1 @@
+python -m torch.distributed.launch --nproc_per_node=8 --use_env main.py --model deit_small_patch16_224 --batch-size 256 --data-path ../../ImageNet --output_dir output
