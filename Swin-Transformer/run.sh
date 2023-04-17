c723aada1af952346d651a01a3468e3beaffb734
tocean <yuxiangyang@microsoft.com>
2023-04-17 06:07:46 +0000

    change to use 8 GPUs in launch script

--- Swin-Transformer/run.sh
+++ Swin-Transformer/run.sh
@@ -1,9 +1,9 @@
 DATA_PATH=../../ImageNet
-export CUDA_VISIBLE_DEVICES=4,5,6,7
+
 python -m torch.distributed.launch \
-    --nproc_per_node 4 \
+    --nproc_per_node 8 \
     --master_port 12345  \
     ../third_party/Swin-Transformer/main.py \
     --cfg ../third_party/Swin-Transformer/configs/swin/swin_tiny_patch4_window7_224.yaml \
     --data-path $DATA_PATH \
-    --batch-size 256
+    --batch-size 128
