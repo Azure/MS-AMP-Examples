7afaebb5c2c0b967aa0a5ca7f10b195c814ed4fa
tocean <yuxiangyang@microsoft.com>
2024-02-26 03:42:04 +0000

    fix bug in v0.4 release

--- RoBERTa/prepare_wikitext.sh
+++ RoBERTa/prepare_wikitext.sh
@@ -10,8 +10,12 @@ then
     rm -rf data-bin
 fi
 
-wget https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-raw-v1.zip
-unzip wikitext-103-raw-v1.zip
+# wget https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-raw-v1.zip
+# unzip wikitext-103-raw-v1.zip
+
+wget https://dax-cdn.cdn.appdomain.cloud/dax-wikitext-103/1.0.1/wikitext-103.tar.gz
+tar -zxvf wikitext-103.tar.gz && mv wikitext-103 wikitext-103-raw
+
 
 mkdir -p gpt2_bpe
 wget -O gpt2_bpe/encoder.json https://dl.fbaipublicfiles.com/fairseq/gpt2_bpe/encoder.json
@@ -23,7 +27,7 @@ for SPLIT in train valid test; do \
     python -m examples.roberta.multiprocessing_bpe_encoder \
         --encoder-json gpt2_bpe/encoder.json \
         --vocab-bpe gpt2_bpe/vocab.bpe \
-        --inputs wikitext-103-raw/wiki.${SPLIT}.raw \
+        --inputs wikitext-103-raw/wiki.${SPLIT}.tokens \
         --outputs wikitext-103-raw/wiki.${SPLIT}.bpe \
         --keep-empty \
         --workers 60; \
@@ -41,4 +45,4 @@ fairseq-preprocess \
     --workers 60
 
 rm -rf gpt2_bpe
-rm -rf wikitext-103-raw*
+rm -rf wikitext-103*
